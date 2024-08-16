57a59f7b7775209f1ae316fd30e0201d7c586209
Iyi-lang <iyiolafash123@gmail.com>
2024-08-16 15:47:03 +0100

    sum function code

---
+++ sumFunction.js
@@ -0,0 +1,7 @@
+function sumOfThreeNumbers(num1, num2, num3) {
+    return num1 + num2 + num3;
+}
+
+// Example usage:
+const result = sumOfThreeNumbers(5, 10, 15);
+console.log("The sum is: " + result);
